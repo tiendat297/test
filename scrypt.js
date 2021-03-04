08f3f9f2d2983382e8cecebc1af05b4ba993232a
Your Name <you@example.com>
2021-03-04 17:15:59 +0700

    update scroll

--- scrypt.js
+++ scrypt.js
@@ -1,13 +1,39 @@
 $(document).ready(
     function() {
-        $('.section--title').waypoint(
+        $('.section_img').waypoint(
             function(direction) {
                 if (direction == "down") {
                     $('nav').addClass('sticky');
+                } else {
+                    $('nav').removeClass('sticky');
                 }
             }, {
-                offset: '100px'
+                offset: '400px'
             }
         )
+    }
+)
+
+$(document).ready(
+    function() {
+        $('.job').waypoint(
+            function(direction) {
+                if (direction == "down") {
+                    $('nav').addClass('sticky');
+                } else {
+                    $('nav').removeClass('sticky');
+                }
+            }, {
+                offset: '400px'
+            }
+        )
+
+        $('a').click(function() {
+            $('html, body').animate({
+                scrollTop: $('[name="' + $.attr(this, 'href').substr(1) + '"]').offset().top
+            }, 500);
+
+            return false;
+        });
     }
 )
